abcd7bd5d9f9c5db894c14d4ac234bfa1dd8c940
rakesh-i <57840815+rakesh-i@users.noreply.github.com>
2021-09-15 01:38:38 +0530

    Persistent TCP connection
    No need for reset after disconnection from client.

--- Files for esp32/server.py
+++ Files for esp32/server.py
@@ -12,36 +12,40 @@ def soc():
     
     #socket setup
     s = socket.socket()         # Create a socket object
-    host = 'Esp32 ip'           # esp32 ip on network
+    host = '192.168.0.110'    # Get local machine name
     port = 12345                # Reserve a port for your service.
     s.bind((host, port))        # Bind to the port
-    s.listen(5)                 # Now wait for client connection.
+    s.listen(5)      # Now wait for client connection.
     
     
-    while True:
-        c, addr = s.accept()    # Establish connection with client.
-        print ('Got connection from', addr)
-        
+    def con(): 
         while True:
-            d = "thank you for connection"
-            data = str(d)
-            msg =str.encode(data, 'utf-8')
-            c.send(msg)
-            a = c.recv(1028)
-            com = a.decode()       
-            de = json.loads(com)#deserialze incoming dictionary
+            c, addr = s.accept()    # Establish connection with client.
+            print ('Got connection from', addr)
             
-            if de["w"] == 1 and de["s"] == 0:
-                motor.motorSpeed(-180)
-            if de["s"] == 1 and de["w"] == 0:
-                motor.motorSpeed(180)
-            if de["a"] == 1 and de["d"] == 0:
-                servo.duty(55)
-            if de["d"] == 1 and de["a"] == 0:
-                servo.duty(89)
-            if de["d"] == 0 and  de["a"] == 0:
-                servo.duty(75)
-            if de["w"] == 0 and de["s"] == 0:
-                motor.motorSpeed(0)
-                
+            while True:
+                d = "thank you for connection"
+                data = str(d)
+                msg =str.encode(data, 'utf-8')
+                try:
+                    c.send(msg)
+                    a = c.recv(1024)
+                    com = a.decode()       
+                    de = json.loads(com)#deserialze incoming dictionary
+                    if de["w"] == 1 and de["s"] == 0:
+                        motor.motorSpeed(-1000)
+                    if de["s"] == 1 and de["w"] == 0:
+                        motor.motorSpeed(1000)
+                    if de["a"] == 1 and de["d"] == 0:
+                        servo.duty(55)
+                    if de["d"] == 1 and de["a"] == 0:
+                        servo.duty(89)
+                    if de["d"] == 0 and  de["a"] == 0:
+                        servo.duty(75)
+                    if de["w"] == 0 and de["s"] == 0:
+                        motor.motorSpeed(0)
+                except:
+                    con()
+    con()
+
             #print(de) #for debugging
