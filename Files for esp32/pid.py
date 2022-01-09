25369fac83788fa96a5eb9eca0981d5d56cec8ac
rakesh-i <57840815+rakesh-i@users.noreply.github.com>
2022-01-09 13:01:20 +0530

    Update pid.py

--- Files for esp32/pid.py
+++ Files for esp32/pid.py
@@ -12,7 +12,7 @@ def soc():
     
     #socket setup
     s = socket.socket()         # Create a socket object
-    host = '192.168.0.110'    # Get local machine name
+    host = '192.168.1.110'    # Get local machine name
     port = 12345                # Reserve a port for your service.
     s.bind((host, port))        # Bind to the port
     s.listen(5)                 # Now wait for client connection.
@@ -21,27 +21,36 @@ def soc():
         return max(min(o_M, (x - i_m) * (o_M - o_m) // (i_M - i_m) + o_m), o_m)
     
     
-    
-    while True:
-        c, addr = s.accept()    # Establish connection with client.
-        print ('Got connection from', addr)
-        
-        while True:
-            d = "thank you for connection"
-            data = str(d)
-            msg =str.encode(data, 'utf-8')
-            c.send(msg)
-            a = c.recv(1024)
-            com = a.decode()       
-            de = json.loads(com)#deserialze incoming dictionary
+    def con():
+    	motor.motorSpeed(0)
+	while True:
+	    c, addr = s.accept()    # Establish connection with client.
+	    print ('Got connection from', addr)
+	    
+		    
+	    while True:
+		d = "thank you for connection"
+		data = str(d)
+		msg =str.encode(data, 'utf-8')
+		try:
+		    c.send(msg)
+		    a = c.recv(1024)
+		    com = a.decode()       
+		    de = json.loads(com)#deserialze incoming dictionary
 
-            angle = int(de['a'])
-            servo.duty(angle)
-            print(angle)
-            if de["w"] > 0:
-                motor.motorSpeed(int(de["w"]))
-            else:
-                motor.motorSpeed(0)
-            
-                
-            #print(de) #for debugging
+		    angle = int(de['a'])
+		    speed = int(de["w"])
+		    servo.duty(angle)
+		    print(angle, speed)
+		    if abs(speed):
+			motor.motorSpeed(speed)
+		    #if speed < 0:
+		    #	motor.motorSpeed(speed)
+		    else:
+			motor.motorSpeed(0)
+				    
+		except:
+		    con()
+     
+    con()    
+				    #print(de) #for debugging
