75cc0cb03b5a65f65b0e65b159a41f63438d0fa3
rakesh-i <57840815+rakesh-i@users.noreply.github.com>
2022-01-10 18:52:26 +0530

    Update log.py

--- Client/log.py
+++ Client/log.py
@@ -11,25 +11,3 @@ def key_check():
         if wapi.GetAsyncKeyState(ord(key)):
             keys.append(key)
     return keys
-
-
-
-#For debugging
-
-#def key_out(key):
-#    output = [0, 0, 0, 0]
-#    if 'A' in key:
-#        output[0] = 1
-#    if 'D' in key:
-#        output[3] = 1
-#    if 'W' in key:
-#        output[1] = 1
-#    if 'S' in key:
-#        output[2] = 1
-#    return output
-
-#while True:
-   # key = key_check()
-    #print(key)
-#    a = key_out(key)
-#    print(a)
