1da723d4c11a2bc924cf744795a628e4b58a83e2
rakesh-i <57840815+rakesh-i@users.noreply.github.com>
2022-01-10 18:37:25 +0530

    Update vision.py

--- OpenCV Line Follower and Trainer/vision.py
+++ OpenCV Line Follower and Trainer/vision.py
@@ -4,24 +4,6 @@ import time
 import os 
 import socket
 
-s = socket.socket()         # Create a socket object
-host = '192.168.0.110' # Get local machine name
-port = 12345                # Reserve a port for your service.
-s.connect((host, port))
-
-
-def key_out(key):
-    output = [0, 0, 0, 0]
-    if 'A' in key:
-        output[0] = 1
-    if 'D' in key:
-        output[2] = 1
-    if 'W' in key:
-        output[1] = 1
-    if 'S' in key:
-        output[3] = 1
-    return output
-
 cap = cv2.VideoCapture('http://192.168.0.104:81/stream')
 
 while True:
