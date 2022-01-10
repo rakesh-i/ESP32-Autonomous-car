ab42ad028d1397adba99414751b78dea65f236d8
rakesh-i <57840815+rakesh-i@users.noreply.github.com>
2022-01-10 18:03:33 +0530

    Update run.py

--- Training/run.py
+++ Training/run.py
@@ -8,10 +8,10 @@ from alexnet import alexnet
 
 
 s = socket.socket()         # Create a socket object
-host = 'esp32 IP'    
+host = '192.168.1.110'    
 port = 12345                # Reserve a port for your service.
 s.connect((host, port))
-cap = cv2.VideoCapture('http://111.111.111.11:11/stream') #esp32cam ip
+cap = cv2.VideoCapture('http://192.168.1.104/stream') #esp32cam ip
 #time.sleep(5)
 
 WIDTH = 80
@@ -19,19 +19,6 @@ HEIGHT = 60
 LR = 1e-3
 EPOCHS = 8
 MODEL_NAME = 'autonomous_car-{}-{}-{}-epochs.model'.format(LR, 'alexnet',EPOCHS)
-
-
-def key_out(key):
-    output = [0, 0, 0, 0]
-    if 'A' in key:
-        output[0] = 1
-    if 'D' in key:
-        output[2] = 1
-    if 'W' in key:
-        output[1] = 1
-    if 'S' in key:
-        output[3] = 1
-    return output
     
 model = alexnet(WIDTH, HEIGHT, LR)
 model.load(MODEL_NAME)
